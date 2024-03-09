50ee1850be1079f793f1850c5f4f25e01d4ea821
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-09 14:26:10 +0530

    Setup Auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello</div>;
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 h-screen w-screen flex items-center">
+        <div className="text-center w-full ">
+          <button onClick={() => signIn("google")}>Sign in</button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <>
+      Signed in as {session.user.email} <br />
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  );
 }
